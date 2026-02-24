f43613556d2191ccae8df71be64e2c98ad5dccbf
Artem Korniienko <tmadmanar2307@gmail.com>
2026-02-24 21:30:51 +0000

    Task 36 [Phase 6: Shutdown & Polish] 5D Cube Support TypoFix

--- scripts/math/shapes/4DCube.gd
+++ scripts/math/shapes/4DCube.gd
@@ -48,19 +48,19 @@ func create_shape() -> ShapeData:
 		data.edges.append(Vector2i(i, i + 8))
 
 	# We define the 6 faces of the inner cube, 6 of outer, and 12 connecting faces
-	#var add_cube_faces = func(offset: int):
-	#	var local_faces = [
-	#		[0, 3, 2, 1], [4, 5, 6, 7], # Back, Front
-	#		[0, 4, 7, 3], [1, 2, 6, 5], # Left, Right
-	#		[3, 7, 6, 2], [0, 1, 5, 4]  # Top, Bottom
-	#	]
-	#	for f in local_faces:
-	#		var new_face = []
-	#		for idx in f: new_face.append(idx + offset)
-	#		data.faces.append(new_face)
-#
-	#add_cube_faces.call(0)
-	#add_cube_faces.call(8)
+	var add_cube_faces = func(offset: int):
+		var local_faces = [
+			[0, 3, 2, 1], [4, 5, 6, 7], # Back, Front
+			[0, 4, 7, 3], [1, 2, 6, 5], # Left, Right
+			[3, 7, 6, 2], [0, 1, 5, 4]  # Top, Bottom
+		]
+		for f in local_faces:
+			var new_face = []
+			for idx in f: new_face.append(idx + offset)
+			data.faces.append(new_face)
+
+	add_cube_faces.call(0)
+	add_cube_faces.call(8)
 
 	var connectors = [
 		[0,1,9,8], [1,2,10,9], [2,3,11,10], [3,0,8,11],
